532874792048e9a1ba63030b3626bfd567e6817b
Alusch <alexdurach@gmail.com>
2022-05-16 18:07:05 +0200

    pics from json included

--- online_shop/src/App.js
+++ online_shop/src/App.js
@@ -4,44 +4,39 @@ const App = () => {
   const categories = [
     {
       id: 1,
-      title: "Computer",
-      image:
-        "https://drive.google.com/file/d/1dFvsX2UwetAYkUF2AEcA916yAYRO15ug/view?usp=sharing",
+      title: "hats",
+      imageUrl: "https://i.ibb.co/cvpntL1/hats.png",
     },
     {
       id: 2,
-      title: "Beamer",
-      image:
-        "https://drive.google.com/file/d/1Xe5tc_sec-aYu2T68YJljkWsJWhbE552/view?usp=sharingg",
+      title: "jackets",
+      imageUrl: "https://i.ibb.co/px2tCc3/jackets.png",
     },
     {
       id: 3,
-      title: "Grafikkarten",
-      image:
-        "https://drive.google.com/file/d/1kHPgiy9Xv4qWAUamyxAIoECj_Yf0PqhR/view?usp=sharing",
+      title: "sneakers",
+      imageUrl: "https://i.ibb.co/0jqHpnp/sneakers.png",
     },
     {
       id: 4,
-      title: "NAS und DAS",
-      image:
-        "https://drive.google.com/file/d/1ciPe4gQ8a7p_54wxKWQfUZ6vMQifR2gB/view?usp=sharing",
+      title: "womens",
+      imageUrl: "https://i.ibb.co/GCCdy8t/womens.png",
     },
     {
       id: 5,
-      title: "Server und Workstations",
-      image:
-        "https://drive.google.com/file/d/1R9t8kWsZ7EfIbn5jPhRKD2K7dqmSK7Ba/view?usp=sharing",
+      title: "mens",
+      imageUrl: "https://i.ibb.co/R70vBrQ/men.png",
     },
   ];
 
   return (
     <div className="categories__container">
-      {categories.map(({ title, id, image }) => (
+      {categories.map(({ title, id, imageUrl }) => (
         <div className="category__container" key={id}>
           <div
-            className="category__image"
+            className="background-image "
             style={{
-              backgroundImage: `url(${image})`,
+              backgroundImage: `url(${imageUrl})`,
             }}
           />
           <div className="category__body">
